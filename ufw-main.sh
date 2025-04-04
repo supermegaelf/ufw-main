a6c479276028e9a7a4a3776511129ec76237d5fe
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-04-04 18:29:43 +0300

    Update ufw-main.sh

--- ufw-main.sh
+++ ufw-main.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+read -p "Node public IP: " node_public_ip
+
 apt update && apt install ufw -y
 
 ufw allow 443/tcp comment "Marzban Dashboard"
+
 ufw allow 22/tcp comment "SSH"
+
+ufw allow from "$node_public_ip" to any port 62050 proto tcp comment 'Marznode'
+
+ufw allow from "$node_public_ip" to any port 62051 proto tcp comment 'Marznode'
 
 ufw enable
